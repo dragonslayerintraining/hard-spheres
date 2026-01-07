bd9fd1d6e881b229b9378caefff12ebbf33b8dd1
Daniel Zhang <22734948+dragonslayerintraining@users.noreply.github.com>
2026-01-07 04:51:52 -0500

    Allow choosing radii for plot

--- README.md
+++ README.md
@@ -8,7 +8,18 @@ For disks of radius 0.1 on the unit torus, it can handle activity up to around 0
 
 The implementation has been tested by using it to estimate the expected size of the random independent set on various 4-vertex graphs and comparing it to the exact value.
 
+
+
 ![Sample from hard disk model at activity 40](hard-disk-radius0.1-activity40.png)
+
+## Example usage
+
+```
+make
+./hard-disk-perfect-sampler 500 0.01 |python3 plot.py 0.01
+```
+
+This generates a sample from the hard disk model with disks of radius 0.01 at activity 500, and plots it.
 
 ## Technical details
 
--- plot.py
+++ plot.py
@@ -4,6 +4,9 @@ import sys
 import time
 import matplotlib.pyplot as plt
 
+radius = 0.1
+if len(sys.argv)>=2:
+    radius = float(sys.argv[1])
 
 plt.figure(figsize=(10,10))
 
@@ -13,12 +16,13 @@ for line in sys.stdin:
     y=arr[1]
     for sx in [-1,0,1]:
         for sy in [-1,0,1]:
-            plt.gca().add_patch(plt.Circle((x+sx,y+sy),0.1,color='b'))
+            plt.gca().add_patch(plt.Circle((x+sx,y+sy),radius,color='b'))
 
 plt.axis([0,1,0,1])
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Points")
+plt.title("Sample from hard disk model")
 
-plt.plot("plot.png")
+#plt.savefig("plot.png")
+plt.plot()
 plt.show()
