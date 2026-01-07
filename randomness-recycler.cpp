d87ffb0b0ecda81ac7e0737076304938f8f65ecf
Daniel Zhang <22734948+dragonslayerintraining@users.noreply.github.com>
2026-01-07 01:43:45 -0500

    Factored out test suite

--- randomness-recycler.cpp
+++ randomness-recycler.cpp
@@ -180,27 +180,40 @@ int main(){
 }
 */
 
-int main(){
+template<class Graph,class RNG>
+double estimate_density(Graph graph,double activity,int num_trials,RNG& rng){
+  int total_size=0;
+  for(int i=1;i<=num_trials;i++){
+    std::vector<typename Graph::Vertex> independent_set=random_independent_set(graph,activity,rng);
+    total_size+=independent_set.size();
+  }
+  return 1.0*total_size/num_trials;
+}
+
+template<class Graph,class RNG>
+void verify_density(Graph graph,double activity,int num_trials,RNG& rng,double truth){
+  double est = estimate_density(graph,activity,num_trials,rng);
+  std::cout<<"Estimate: "<<est<<" Truth: "<<truth<<std::endl;
+  assert(std::abs(est-truth)<0.01);
+}
+
+void test_discrete(){
+  //Estimate the expected size of the independent set by Monte Carlo and check that it is close to ground truth
+
   std::random_device rd;
   std::mt19937 rng(rd());
-  //DiscreteGraph graph(4,{});//2.0
-  //DiscreteGraph graph(4,{{0,1}});//1.666
-  //DiscreteGraph graph(4,{{0,1},{0,2},{1,2}});//1.25
-  //DiscreteGraph graph(4,{{0,1},{2,3}});//1.333
-  //DiscreteGraph graph(4,{{0,1},{1,2},{2,3}});//1.25
-  //DiscreteGraph graph(4,{{0,1},{0,2},{0,3}});//1.44
-  //DiscreteGraph graph(4,{{0,1},{1,2},{2,3},{3,0}});//1.143
-  DiscreteGraph graph(4,{{0,1},{0,2},{0,3},{1,2},{1,3},{2,3}});//0.80
-  int total_size=0;
-  for(int i=1;i<=10000000;i++){
-    std::vector<typename DiscreteGraph::Vertex> independent_set=random_independent_set(graph,1.0,rng);
-    total_size+=independent_set.size();
-    if(i%100000==0){
-      std::cout<<"Iteration "<<i<<std::endl;
-      for(DiscreteGraph::Vertex p:independent_set){
-	std::cout<<p.id<<std::endl;
-      }
-      std::cout<<"Average size: "<<(double)total_size/i<<std::endl;
-    }
-  }
+  
+  verify_density(DiscreteGraph(4,{}),1.0,1000000,rng,2.0);
+  verify_density(DiscreteGraph(4,{{0,1}}),1.0,1000000,rng,5.0/3);
+  verify_density(DiscreteGraph(4,{{0,1},{0,2},{1,2}}),1.0,1000000,rng,1.25);
+  verify_density(DiscreteGraph(4,{{0,1},{2,3}}),1.0,1000000,rng,4.0/3);
+  verify_density(DiscreteGraph(4,{{0,1},{1,2},{2,3}}),1.0,1000000,rng,1.25);
+  verify_density(DiscreteGraph(4,{{0,1},{0,2},{0,3}}),1.0,1000000,rng,13.0/9);
+  verify_density(DiscreteGraph(4,{{0,1},{1,2},{2,3},{3,0}}),1.0,1000000,rng,8.0/7);
+  verify_density(DiscreteGraph(4,{{0,1},{0,2},{0,3},{1,2},{1,3},{2,3}}),1.0,1000000,rng,0.80);
+  verify_density(DiscreteGraph(4,{{0,1},{1,2},{2,3},{3,0}}),2.0,10000,rng,24.0/17);
+}
+
+int main(){
+  test_discrete();
 }
