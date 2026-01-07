7ff6061a8ecccfad6782e86eb18214be1a78db3b
Daniel Zhang <22734948+dragonslayerintraining@users.noreply.github.com>
2026-01-06 23:41:36 -0500

    Renamed Point to Vertex

--- randomness-recycler.cpp
+++ randomness-recycler.cpp
@@ -7,40 +7,33 @@
 #include <vector>
 
 
-/* region is [0,1]x[0,1] */
-//const double target_lambda = 100.0;
-//const double range = 2*0.1;
-
-std::random_device rd;
-std::mt19937 rng(rd());
-
-
-//Graph whose vertex set is 
 class HardDiskGraph{
   double range;
 public:
-  struct Point{double x,y;};
-  friend bool operator<(Point p,Point q){
+  struct Vertex{double x,y;};
+  friend bool operator<(Vertex p,Vertex q){
     return std::make_pair(p.x,p.y)<std::make_pair(q.x,q.y);
   }
-  friend std::ostream& operator<<(std::ostream& stream,const Point& p){
+  friend std::ostream& operator<<(std::ostream& stream,const Vertex& p){
     return stream<<"("<<p.x<<","<<p.y<<")";
   }
   HardDiskGraph(double radius):range(radius*2){
   }
-  Point random_vertex() const{
+  template<class RNG>
+  Vertex random_vertex(RNG& rng) const{
     std::uniform_real_distribution<> distr(0,1);
     double x=distr(rng);
     double y=distr(rng);
-    return Point{x,y};
+    return Vertex{x,y};
   }
   //total measure of vertices
   double size() const{
     return 1.0;
   }
-  //https://mathworld.wolfram.com/DiskPointPicking.html
+  //https://mathworld.wolfram.com/DiskVertexPicking.html
   //Output a uniformly random point from B(p,2r)
-  Point random_neighbor(Point p) const{
+  template<class RNG>
+  Vertex random_neighbor(Vertex p,RNG& rng) const{
     std::uniform_real_distribution<> distr(0,1);
     double s=std::sqrt(distr(rng));
     double theta=distr(rng)*2*std::numbers::pi;
@@ -50,14 +43,14 @@ public:
     while(x>1) x-=1;
     while(y<0) y+=1;
     while(y>0) y-=1;
-    return Point{x,y};
+    return Vertex{x,y};
   }
-  double degree(Point p) const{
+  double degree(Vertex p) const{
     return std::numbers::pi*range*range;
   }
 
   //Returns whether two points are adjacent (i.e. within 2r on the torus)
-  bool is_adj(Point p,Point q) const{
+  bool is_adj(Vertex p,Vertex q) const{
     double dx=std::abs(p.x-q.x),dy=std::abs(p.y-q.y);
     dx=std::min(dx,1-dx);
     dy=std::min(dy,1-dy);
@@ -67,14 +60,14 @@ public:
 
 //Generate a random independent set distributed according to the hard core model on [0,1]x[0,1] where points of distance <range are adjacent
 template<class Graph>
-std::vector<typename Graph::Point> random_independent_set(const Graph graph,const double target_lambda){
-  std::set<std::pair<double,typename Graph::Point> > independent_set;
+std::vector<typename Graph::Vertex> random_independent_set(const Graph& graph,const double target_lambda){
+  std::set<std::pair<double,typename Graph::Vertex> > independent_set;
 
   //"dents" in the activity:
   // The ball of radius 2r around the point is reduced to this value
-  std::set<std::pair<double,typename Graph::Point> > dents;
+  std::set<std::pair<double,typename Graph::Vertex> > dents;
   double lambda=0;
-  auto try_add=[&](Graph::Point p,double lam){
+  auto try_add=[&](Graph::Vertex p,double lam){
     std::cerr<<"try_add"<<std::endl;
     for(const auto& [l,q]:independent_set | std::views::reverse){
       std::cerr<<l<<" "<<q<<std::endl;
@@ -90,10 +83,12 @@ std::vector<typename Graph::Point> random_independent_set(const Graph graph,cons
     independent_set.insert({lam,p});
   };
   
+  std::random_device rd;
+  std::mt19937 rng(rd());
   while(true){
     while(dents.size()>=1){
       //lift the lowest dent
-      std::pair<double,typename Graph::Point> lowest_dent=*dents.begin();
+      std::pair<double,typename Graph::Vertex> lowest_dent=*dents.begin();
       dents.erase(dents.begin());
       
       std::exponential_distribution<> next_for_vertex(graph.degree(lowest_dent.second));
@@ -102,7 +97,7 @@ std::vector<typename Graph::Point> random_independent_set(const Graph graph,cons
 	continue;
       }
       dents.insert(lowest_dent);
-      typename Graph::Point p = graph.random_neighbor(lowest_dent.second);
+      typename Graph::Vertex p = graph.random_neighbor(lowest_dent.second,rng);
       try_add(p,lowest_dent.first);
     }
     //no dents
@@ -111,10 +106,10 @@ std::vector<typename Graph::Point> random_independent_set(const Graph graph,cons
     if(lambda>target_lambda){
       break;
     }
-    typename Graph::Point p = graph.random_vertex();
+    typename Graph::Vertex p = graph.random_vertex(rng);
     try_add(p,lambda);
   }
-  std::vector<typename Graph::Point> output;
+  std::vector<typename Graph::Vertex> output;
   for(const auto& [l,p]:independent_set){
     output.push_back(p);
   }
@@ -123,8 +118,8 @@ std::vector<typename Graph::Point> random_independent_set(const Graph graph,cons
 
 int main(){
   HardDiskGraph graph(0.1);//discs of radius 0.1 on [0,1]x[0,1]
-  std::vector<typename HardDiskGraph::Point> independent_set=random_independent_set(graph,100.0);
-  for(HardDiskGraph::Point p:independent_set){
+  std::vector<typename HardDiskGraph::Vertex> independent_set=random_independent_set(graph,100.0);
+  for(HardDiskGraph::Vertex p:independent_set){
     std::cout<<p.x<<" "<<p.y<<std::endl;
   }
 }
